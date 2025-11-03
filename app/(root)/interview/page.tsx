3fe8ed22aca3f267f474d2f9c5f77e2a54513777
DamnX9211 <rohitkuumar1995@gmail.com>
2025-11-03 12:40:59 +0530

    added interview generation UI

---
+++ app/(root)/interview/page.tsx
@@ -0,0 +1,13 @@
+import Agent from '@/components/Agent'
+import React from 'react'
+
+const page = () => {
+  return (
+    <>
+    <h1>Interview Generation</h1>
+    <Agent />
+    </>
+  )
+}
+
+export default page
